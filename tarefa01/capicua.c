c8a8202aa5fcc9f481f7eeeff84800ac53903947
Kauã Rodrigues <281279@dac.unicamp.br>
2024-08-15 23:24:07 -0300

    Adição do exercício capicua.c

---
+++ tarefa01/capicua.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#define MAX 10000000000
+
+unsigned long long reverse_num(unsigned long long k) {
+    unsigned long long i=10, reverse=0, aux_k=k;
+
+    for (; i<MAX && aux_k!=0;) {
+        reverse=reverse*10+aux_k%i;
+        aux_k=(aux_k-aux_k%i)/10;
+    };
+
+    return reverse;
+}
+
+int is_palindrome(unsigned long long k) {
+    if (k == reverse_num(k)) {
+        return 1;
+    }
+
+    return 0;
+}
+
+int main() {
+    int n;
+    long long k;
+
+    scanf("%d", &n);
+
+    for (int i=0; i<n; i++) {
+        scanf("%lld", &k);
+        if (is_palindrome(k)) {
+            printf("%lld eh capicua\n", k);
+        }
+        else {
+            printf("%lld nao eh capicua\n", k);
+        }
+    }
+
+    return 0;
+}
