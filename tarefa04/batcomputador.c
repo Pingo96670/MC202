fc860e1f3c0ce1abe035050e69f723dc40a37340
Kauã Rodrigues <281279@dac.unicamp.br>
2024-09-16 18:35:25 -0300

    Limpeza e reorganização geral do código, adição de comentários nas funções e remoção de função debug_print usada para testes

--- tarefa04/batcomputador.c
+++ tarefa04/batcomputador.c
@@ -2,50 +2,15 @@
 #include <string.h>
 #include <stdlib.h>
 
+
 typedef struct mem {
     int size;
     int *num_array;
     int free;
 } mem;
 
-// TODO
-// All
-    // Reorganize arguments order
-    // Cleanup
 
-void debug_print(mem *bat_mem) {
-    for (int i=0; i<bat_mem->size; i++) {
-        printf("%d ", bat_mem->num_array[i]);
-    }
-}
-
-int can_cleanup(mem *bat_mem) {
-    int i;
-
-    for (i=0; i<bat_mem->size/4; i++) {
-        if (bat_mem->num_array[i]==-1) {
-            return 0;
-        }
-    }
-
-    return 1;
-}
-
-void mem_cleanup(mem *bat_mem) {
-    int i=0, *temp_array;
-
-    temp_array=bat_mem->num_array;
-
-    bat_mem->free-=bat_mem->size*1/2;
-    bat_mem->size/=2;
-
-    bat_mem->num_array=malloc(bat_mem->size*sizeof(int));
-
-    for (; i<bat_mem->size; i++) {
-        bat_mem->num_array[i]=temp_array[i];
-    }
-}
-
+// Function to expand memory, doubling its size
 void double_mem(mem *bat_mem) {
     int i=0, *temp_array;
 
@@ -53,8 +18,6 @@ void double_mem(mem *bat_mem) {
 
     bat_mem->free+=bat_mem->size;
     bat_mem->size*=2;
-
-    //free(bat_mem->num_array);
 
     bat_mem->num_array=malloc(bat_mem->size*sizeof(int));
 
@@ -69,6 +32,8 @@ void double_mem(mem *bat_mem) {
     free(temp_array);
 }
 
+// Function to find the first free continuous slot in memory with specified size
+// Returns -1 if none are found
 int find_free(mem* bat_mem, int num_search) {
     int i=0, count=0;
 
@@ -77,10 +42,8 @@ int find_free(mem* bat_mem, int num_search) {
             count++;
 
             if (count==num_search) {
-                //return bat_mem->num_array[i-num_search+1];
                 return i-num_search+1;
             }
-
         } else {
             count=0;
         }
@@ -89,7 +52,10 @@ int find_free(mem* bat_mem, int num_search) {
     return -1;
 }
 
-void bat_alloc(int n_int, mem *bat_mem) {
+// Bat_alloc command
+// Allocates memory for an array, containing [size + data]
+// Runs double_mem if needed
+void bat_alloc(mem *bat_mem, int n_int) {
     int i, temp_int, start_address;
 
     if (n_int+1>bat_mem->free || find_free(bat_mem, n_int+1)==-1) {
@@ -111,6 +77,38 @@ void bat_alloc(int n_int, mem *bat_mem) {
     printf("%d\n", start_address);
 }
 
+// Function to check if a function can be cleaned up
+int can_cleanup(mem *bat_mem) {
+    int i;
+
+    for (i=0; i<bat_mem->size/4; i++) {
+        if (bat_mem->num_array[i]==-1) {
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
+// Function to free memory, reducing size by 1/2
+void mem_cleanup(mem *bat_mem) {
+    int i=0, *temp_array;
+
+    temp_array=bat_mem->num_array;
+
+    bat_mem->free-=bat_mem->size*1/2;
+    bat_mem->size/=2;
+
+    bat_mem->num_array=malloc(bat_mem->size*sizeof(int));
+
+    for (; i<bat_mem->size; i++) {
+        bat_mem->num_array[i]=temp_array[i];
+    }
+}
+
+// Bat_free command
+// Frees up an array in memory from a specified address
+// Runs can_cleanup and mem_cleanup if possible
 void bat_free(mem *bat_mem, int address) {
     int n, i, mem_used;
 
@@ -132,6 +130,8 @@ void bat_free(mem *bat_mem, int address) {
     }
 }
 
+// Bat_print command
+// Prints the array stored in the specified position
 void bat_print(mem *bat_mem, int address) {
     int i, n;
 
@@ -144,12 +144,16 @@ void bat_print(mem *bat_mem, int address) {
     printf("\n");
 }
 
+// Bat_usage command
+// Prints out used/total memory
 void bat_usage(mem *bat_mem) {
     int used=bat_mem->size-bat_mem->free;
 
     printf("%d de %d\n", used, bat_mem->size);
 }
 
+
+// Main
 int main() {
     int n, i, n_int, address;
     mem bat_mem;
@@ -172,7 +176,7 @@ int main() {
         if (strcmp(command, "bat-alloc")==0) {
             scanf("%d", &n_int);
 
-            bat_alloc(n_int, &bat_mem);
+            bat_alloc(&bat_mem, n_int);
 
         } else if (strcmp(command, "bat-free")==0) {
             scanf("%d", &address);
@@ -187,14 +191,10 @@ int main() {
         } else if (strcmp(command, "bat-uso")==0) {
             bat_usage(&bat_mem);
 
-        } else if (strcmp(command, "debug")==0) {
-            debug_print(&bat_mem);
-
         } else {
             printf("Comando nao reconhecido");
             break;
-        }
-        
+        } 
     }
 
     return 0;
