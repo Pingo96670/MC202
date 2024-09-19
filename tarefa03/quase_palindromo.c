3cf1cd6d7b44cb378da3a3ef6c58649a67bdc3c1
Kauã Rodrigues <281279@dac.unicamp.br>
2024-09-18 22:58:12 -0300

    quase_palindromo.c: alteração no nome usado no #define de MAX para MAX_WORD_LENGTH para maior clareza e adição de comentários na função recursiva is_k_quasi_palindrome recompensa.c: alteração de nome da função e adição de comentários na função recursiva dice_roll_prob_tree para maior clareza e remoção de variável desnecessária chance

--- tarefa03/quase_palindromo.c
+++ tarefa03/quase_palindromo.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <string.h>
-#define MAX 400
+#define MAX_WORD_LENGTH 400
 
 // Function to determine if a word is a k-quasi-palindrome
+// Uses recursion to check words in "a-p-a" manner, where "p" is a palindrome and "a" is any letter
+// Returns 0 if there are more mismatches (k_count) than requested (k)
+// Returns 1 otherwise, reaching the end with k_count<=k
 int is_k_quasi_palindrome(char *word, int k, int start, int end, int k_count) {
 
     if (start>=end) {
@@ -22,7 +25,7 @@ int is_k_quasi_palindrome(char *word, int k, int start, int end, int k_count) {
 
 int main() {
     int k, k_count=0;
-    char word[MAX];
+    char word[MAX_WORD_LENGTH];
 
     scanf("%d %s", &k, word);
 
--- tarefa03/recompensa.c
+++ tarefa03/recompensa.c
@@ -1,33 +1,38 @@
 #include <stdio.h>
 
 // Recursive function to calculate probability of receiving the reward
+// Checks odds in a "tree-like" manner, which ends up going through all possible roll combinations that reach the given sum x in n or less rolls
+// When all recursive calls reach an endpoint, adds up each "branch"'s probability, which is finally returned to main call
+
 // There may be a much more efficient method
-float dice_roll(int n, int x, float *chance) {
+float dice_roll_prob_tree(int n, int x) {
+    // Roll combinations exceed x or n attempts, returning 0
     if (x<0 || n<0) {
         return 0;
     }
 
+    // Roll combinations reach x, returning 1, which is then multiplied by 1/6 in parent call for actual odds
     else if (x==0) {
         return 1;
     }
 
+    // Checks all possible results for the following roll
     else {
-        return 1.0/6.0*(dice_roll(n-1, x-1, chance)+
-                        dice_roll(n-1, x-2, chance)+
-                        dice_roll(n-1, x-3, chance)+
-                        dice_roll(n-1, x-4, chance)+
-                        dice_roll(n-1, x-5, chance)+
-                        dice_roll(n-1, x-6, chance));
+        return 1.0/6.0*(dice_roll_prob_tree(n-1, x-1)+
+                        dice_roll_prob_tree(n-1, x-2)+
+                        dice_roll_prob_tree(n-1, x-3)+
+                        dice_roll_prob_tree(n-1, x-4)+
+                        dice_roll_prob_tree(n-1, x-5)+
+                        dice_roll_prob_tree(n-1, x-6));
     }
 }
 
 int main() {
     int n, x;
-    float chance=0;
 
     scanf("%d %d", &n, &x);
 
-    printf("%.3f", dice_roll(n, x, &chance));
+    printf("%.3f", dice_roll_prob_tree(n, x));
 
     return 0;
 }
