31cd621592b704e9f7e26924ba82beb0fe0a0352
Kauã Rodrigues <281279@dac.unicamp.br>
2024-09-30 17:50:26 -0300

    Implementação inicial de collision_check e adição do atributo is_head à struct node, atribuído valor 1 no nó retornado pela função create_head

--- tarefa06/cobra.c
+++ tarefa06/cobra.c
@@ -11,6 +11,7 @@ typedef struct pos {
 typedef struct node_type *node;
 struct node_type {
     pos position;
+    int is_head;
     node previous;
     node next;
 };
@@ -19,11 +20,15 @@ struct node_type {
 node create_head(int head_x, int head_y) {
     node head;
 
+    head=malloc(sizeof(struct node_type));
+
     head->next=head;
     head->previous=head;
 
     head->position.x=head_x;
     head->position.y=head_y;
+
+    head->is_head=1;
 
     return head;
 }
@@ -57,9 +62,31 @@ void move(node head, int move_x, int move_y) {
     }
 }
 
-void eat_fruit(node head);
+int collision_check(node head, pos fruit) {
+    int i;
+    node current_node;
+    current_node=head->next;
 
-void collision_check(node head);
+    if (head->position.x==fruit.x && head->position.y==fruit.y) {
+        eat_fruit(head)
+    }
+
+    while (!current_node->is_head) {
+        if (head->position.x==current_node->position.x && head->position.y==current_node->position.y) {
+            return 1;
+        } else {
+            current_node=current_node->next;
+        }
+    }
+
+    return 0;
+}
+
+void eat_fruit(node head) {
+    if 
+}
+
+
 
 void print_board();
 
