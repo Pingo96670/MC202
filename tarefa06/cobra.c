67abcf9359b7dd20abd5d11ce2976088277821ae
Kauã Rodrigues <281279@dac.unicamp.br>
2024-10-01 00:57:26 -0300

    Limpeza do código, adição de comentários e definição dos valores ASCII dos caracteres como seus nomes para melhor legibilidade

--- tarefa06/cobra.c
+++ tarefa06/cobra.c
@@ -2,13 +2,19 @@
 #include <stdlib.h>
 #include <string.h>
 #define MAX_COMMAND_SIZE 10
+#define HASH 35
+#define ASTERISK 42
+#define UNDERSCORE 95
 
+
+// Struct do tabuleiro, contendo dimensões e uma matriz com os caracteres de cada célula
 typedef struct board {
     char **data;
     int size_x;
     int size_y;
 } board;
 
+// Struct de posição (x, y)
 typedef struct pos {
     int x;
     int y;
@@ -23,6 +29,8 @@ struct node_type {
     node next;
 };
 
+
+// Função para a operação módulo (formato a mod b)
 int mod(int a, int b) {
     if (a>=0) {
         return a%b;
@@ -31,6 +39,7 @@ int mod(int a, int b) {
     return b+a%b;
 }
 
+// Função para criar uma matriz i×j, atribuindo o caractere "_" (ASCII 95) a todas as células
 char **create_matrix(int i, int j) {
     int k, l;
     char **matrix;
@@ -41,14 +50,15 @@ char **create_matrix(int i, int j) {
         matrix[k]=malloc(j*sizeof(char));
 
         for (l=0; l<j; l++) {
-            matrix[k][l]=95;
+            matrix[k][l]=UNDERSCORE;
         }
     }
 
     return matrix;
 }
 
-
+// Função para criar um nó relacionado à cabeça da cobra na posição (head_x, head_y)
+// Retorna o nó head
 node create_head(int head_x, int head_y) {
     node head;
 
@@ -65,6 +75,7 @@ node create_head(int head_x, int head_y) {
     return head;
 }
 
+// Função para criar a fruta na posição (head_x, head_y), que é então retornada como pos
 pos create_fruit(int fruit_x, int fruit_y) {
     pos fruit;
 
@@ -74,6 +85,8 @@ pos create_fruit(int fruit_x, int fruit_y) {
     return fruit;    
 }
 
+// Função utilizada ao comer uma fruta
+// Cria um novo nó entre a cabeça e o nó seguinte
 void eat_fruit(node head, int move_x, int move_y, board board) {
     node new;
 
@@ -82,9 +95,7 @@ void eat_fruit(node head, int move_x, int move_y, board board) {
     new->is_head=0;
     
     new->position=head->position;
-
-    
-    
+  
     new->previous=head;
     new->next=head->next;
     
@@ -95,44 +106,44 @@ void eat_fruit(node head, int move_x, int move_y, board board) {
     head->position.y=abs(mod(head->position.y+move_y, board.size_y));
 }
 
+// Função para mover a cobra e checar se come uma fruta
+// O "movimento" consiste em manipular o nó relacionado à cauda da cobra para ser o nó após a cabeça (ocupando a posição que era a cabeça antes do movimento)
 void move(node head, int move_x, int move_y, pos fruit, int *n_eaten, board board) {
     node temp_node=head->previous;
 
     if (abs(mod(head->position.x+move_x, board.size_x))==fruit.x && abs(mod(head->position.y+move_y, board.size_y))==fruit.y) {
         *n_eaten+=1;
-        
         eat_fruit(head, move_x, move_y, board);
 
     } else {
-
-
         head->previous->position=head->position;
 
         head->position.x=abs(mod(head->position.x+move_x, board.size_x));
         head->position.y=abs(mod(head->position.y+move_y, board.size_y));
 
-        if (head->previous!=NULL) {
-            head->previous->previous->next=head;
-            head->previous=head->previous->previous;
+        head->previous->previous->next=head;
+        head->previous=head->previous->previous;
 
-            temp_node->previous=head;
-            temp_node->next=head->next;
+        temp_node->previous=head;
+        temp_node->next=head->next;
 
-            head->next->previous=temp_node;
-            head->next=temp_node;
-        }
+        head->next->previous=temp_node;
+        head->next=temp_node;
+        
     }
 }
 
+// Função para checar se a cobra colide com seu corpo
+// Checa se a célula para qual a cobra irá se mover contém uma parte de seu corpo (#)
 int self_collision_check(node head, int move_x, int move_y, board board) {
-    if (board.data[abs(mod(head->position.y+move_y, board.size_y))][abs(mod(head->position.x+move_x, board.size_x))]==35) {
+    if (board.data[abs(mod(head->position.y+move_y, board.size_y))][abs(mod(head->position.x+move_x, board.size_x))]==HASH) {
         return 1;
     }
 
     return 0;
 }
 
-
+// Função para imprimir o tabuleiro
 void print_board(board board) {
     int i, j;
 
@@ -145,6 +156,7 @@ void print_board(board board) {
     }
 }
 
+// Função para liberar a memória alocada à cobra e ao tabuleiro
 void free_game(node head, board board) {
     int i;
     node current_node, next_node;
@@ -168,6 +180,7 @@ void free_game(node head, board board) {
 }
 
 
+// Função main
 int main() {
     int i, j, game_end=0, win=0, n_eaten=0;
     char command[MAX_COMMAND_SIZE];
@@ -184,8 +197,9 @@ int main() {
     // Cria e posiciona a cobra
     scanf("%s %d %d", command,&i, &j);
     head=create_head(j, i);
-    board.data[i][j]=35;
+    board.data[i][j]=HASH;    // Coloca o caractete # (ASCII 35) na posição correta do tabuleiro
 
+    // Loop do jogo
     while (!game_end) {
         scanf("%s", command);
 
@@ -194,52 +208,50 @@ int main() {
             scanf("%d %d", &i, &j);
 
             fruit=create_fruit(j, i);
+            board.data[i][j]=ASTERISK;    // Coloca o caractete * (ASCII 42) na posição correta do tabuleiro
 
-            board.data[i][j]=42;
 
         } else if (strcmp(command, "w")==0) {
             game_end=self_collision_check(head, 0, -1, board);
 
             if (!(head->position.x==fruit.x && abs(mod(head->position.y-1,board.size_y))==fruit.y)) {
-                board.data[head->previous->position.y][head->previous->position.x]=95;
+                board.data[head->previous->position.y][head->previous->position.x]=UNDERSCORE;
             }
-            board.data[abs(mod(head->position.y-1, board.size_y))][head->position.x]=35;
 
-            
-
+            board.data[abs(mod(head->position.y-1, board.size_y))][head->position.x]=HASH;
             move(head, 0, -1, fruit, &n_eaten, board);
 
-        } else if (strcmp(command, "a")==0) {
 
+        } else if (strcmp(command, "a")==0) {
             game_end=self_collision_check(head, -1, 0, board);
 
             if (!(abs(mod(head->position.x-1, board.size_x))==fruit.x && head->position.y==fruit.y)) {
-                board.data[head->previous->position.y][head->previous->position.x]=95;
+                board.data[head->previous->position.y][head->previous->position.x]=UNDERSCORE;
             }
-            board.data[head->position.y][abs(mod(head->position.x-1, board.size_x))]=35;
 
+            board.data[head->position.y][abs(mod(head->position.x-1, board.size_x))]=HASH;
             move(head, -1, 0, fruit, &n_eaten, board);
 
-        } else if (strcmp(command, "s")==0) {
 
+        } else if (strcmp(command, "s")==0) {
             game_end=self_collision_check(head, 0, 1, board);
 
             if (!(head->position.x==fruit.x && (head->position.y+1)%board.size_y==fruit.y)) {
-                board.data[head->previous->position.y][head->previous->position.x]=95;
+                board.data[head->previous->position.y][head->previous->position.x]=UNDERSCORE;
             }
-            board.data[(head->position.y+1)%board.size_y][head->position.x]=35;
 
+            board.data[(head->position.y+1)%board.size_y][head->position.x]=HASH;
             move(head, 0, 1, fruit, &n_eaten, board);
 
-        } else if (strcmp(command, "d")==0) {
 
+        } else if (strcmp(command, "d")==0) {
             game_end=self_collision_check(head, 1, 0, board);
 
             if (!((head->position.x+1)%board.size_x==fruit.x && head->position.y==fruit.y)) {
-                board.data[head->previous->position.y][head->previous->position.x]=95;
+                board.data[head->previous->position.y][head->previous->position.x]=UNDERSCORE;
             }
-            board.data[head->position.y][(head->position.x+1)%board.size_x]=35;
 
+            board.data[head->position.y][(head->position.x+1)%board.size_x]=HASH;
             move(head, 1, 0, fruit, &n_eaten, board);
 
 
@@ -251,6 +263,7 @@ int main() {
             print_board(board);
         }
 
+        // Checa se a quantidade máxima de frutas foi obtida e encerra o jogo
         if (n_eaten==board.size_x*board.size_y-1) {
             game_end=1;
             win=1;
@@ -259,6 +272,7 @@ int main() {
         printf("\n");
     }
 
+    // Fim de jogo
     if (win) {
         printf("YOU WIN");
     } else {
