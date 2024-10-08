438736c2a86cdc214fe3cbefa13b63e69a1dbf16
Kauã Rodrigues <281279@dac.unicamp.br>
2024-10-08 05:44:38 -0300

    Adição de comentários e lista TODO

--- tarefa07/otimizar.c
+++ tarefa07/otimizar.c
@@ -3,16 +3,22 @@
 #include <string.h>
 #define MAX_STR_LEN 4096
 
+// TODO
+// Alocação dinâmica para o input
+// Comentários mais definidos
+// Outras correções apontadas na correção
+
 // Struct de um nó para a árvore
 typedef struct node_type *node;
 struct node_type {
     char data;
-    //node parent;
     node left;
     node right;
     int is_negative;
 };
 
+
+// Função recursiva para liberar uma árvore binária em pós-ordem
 void free_tree(node root) {
     if (root->left!=NULL) {
         free_tree(root->left);
@@ -25,6 +31,8 @@ void free_tree(node root) {
     free(root);
 }
 
+// Função para ler uma expressão recursivamente e armazená-la como árvore binária
+// Retorna a raiz da árvore principal
 node read_expression(char expression[], int *reading_pos, int is_negative) {
     int negative_count=0;
     char current;
@@ -53,7 +61,7 @@ node read_expression(char expression[], int *reading_pos, int is_negative) {
         parent_temp->left=left_temp;
         parent_temp->right=right_temp;
 
-        // Skips the character ")"
+        // Pula o caractere ")"
         *reading_pos+=1;
 
         return parent_temp;
@@ -69,10 +77,12 @@ node read_expression(char expression[], int *reading_pos, int is_negative) {
     }
 }
 
+// Função suporte para inverter o atributo is_negative de um nó
 void invert_negative(int *is_negative) {
     *is_negative=(*is_negative+1)%2;
 }
 
+// Função para aplicar o Teorema de De Morgan sobre a árvore em pré-ordem
 void de_morgan(node root) {
     if (root->is_negative && root->data=='&') {
         root->data='|';
@@ -86,7 +96,6 @@ void de_morgan(node root) {
         invert_negative(&(root->right->is_negative));
     }
 
-
     if (root->left!=NULL) {
         de_morgan(root->left);
     }
@@ -97,6 +106,7 @@ void de_morgan(node root) {
 
 }
 
+// Função para checar se duas expressões são equivalentes
 int is_equivalent(node root_expression1, node root_expression2) {
     if (root_expression1->data!='&' && root_expression1->data!='|' && root_expression2->data!='&' && root_expression2->data!='|') {
         return ((root_expression1->data==root_expression2->data) && (root_expression1->is_negative==root_expression2->is_negative));
@@ -109,6 +119,7 @@ int is_equivalent(node root_expression1, node root_expression2) {
     }
 }
 
+// Função para simplificar uma expressão, percorrendo a árvore em pós-ordem
 void simplify_expression(node root) {
     node temp_node;
 
@@ -196,6 +207,7 @@ void simplify_expression(node root) {
     }
 }
 
+// Função para imprimir uma expressão, percorrendo a árvore em in-ordem
 void print_tree(node root) {
     if (root!=NULL) {
         if(root->is_negative) {
@@ -214,6 +226,7 @@ void print_tree(node root) {
 }
 
 
+// Função main
 int main() {
     int reading_pos=0;
     node tree;
