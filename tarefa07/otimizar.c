0c82b663a53a6f9536519867d3d1955ec245e273
Kauã Rodrigues <281279@dac.unicamp.br>
2024-10-08 05:25:18 -0300

    Correções/alterações: MAX_STR_LEN: Aumentado de 1024 para 4096 read_expression: Pequena correção de um erro de fim de função sem retorno simplify_root: Alterações na ordem de troca de ponteiros, adicionada a transferência do atributo is_negative e liberação de nós não mais utilizados main: Liberação da árvore principal tree
    Adições:
    free_tree: Implementação inicial
    is_equivalent: Implementação com funcionalidade

--- tarefa07/otimizar.c
+++ tarefa07/otimizar.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define MAX_STR_LEN 1024
+#define MAX_STR_LEN 4096
 
 // Struct de um nó para a árvore
 typedef struct node_type *node;
@@ -13,6 +13,17 @@ struct node_type {
     int is_negative;
 };
 
+void free_tree(node root) {
+    if (root->left!=NULL) {
+        free_tree(root->left);
+    }
+
+    if (root->right!=NULL) {
+        free_tree(root->right);
+    }
+
+    free(root);
+}
 
 node read_expression(char expression[], int *reading_pos, int is_negative) {
     int negative_count=0;
@@ -20,7 +31,6 @@ node read_expression(char expression[], int *reading_pos, int is_negative) {
     node parent_temp, left_temp, right_temp, current_node_temp;
 
     current=expression[*reading_pos];
-
 
     while (current=='!') {
         negative_count++;
@@ -43,13 +53,12 @@ node read_expression(char expression[], int *reading_pos, int is_negative) {
         parent_temp->left=left_temp;
         parent_temp->right=right_temp;
 
-        // Pula o caractere ")"
+        // Skips the character ")"
         *reading_pos+=1;
 
         return parent_temp;
-    }
 
-    if (current!='(' && current!=')') {
+    } else {
         current_node_temp=malloc(sizeof(struct node_type));
 
         current_node_temp->data=current;
@@ -88,10 +97,17 @@ void de_morgan(node root) {
 
 }
 
-int is_equivalent(node root_exp1, node root_exp2) {
-
-    return 0;
+int is_equivalent(node root_expression1, node root_expression2) {
+    if (root_expression1->data!='&' && root_expression1->data!='|' && root_expression2->data!='&' && root_expression2->data!='|') {
+        return ((root_expression1->data==root_expression2->data) && (root_expression1->is_negative==root_expression2->is_negative));
+        
+    } else if ((root_expression1->data=='&' && root_expression2->data=='&') || (root_expression1->data=='|' && root_expression2->data=='|')) {
+        return ((is_equivalent(root_expression1->left, root_expression2->left) && is_equivalent(root_expression1->right, root_expression2->right)) ||
+                (is_equivalent(root_expression1->left, root_expression2->right) && is_equivalent(root_expression1->right, root_expression2->left)));
+    } else {
+        return 0;
     }
+}
 
 void simplify_expression(node root) {
     node temp_node;
@@ -124,23 +140,29 @@ void simplify_expression(node root) {
             } else if (root->left->data=='T') {
                 temp_node=root->right;
 
+                free(root->left);
+
                 root->data=root->right->data;
                 root->left=root->right->left;
+                root->is_negative=root->right->is_negative;
                 root->right=root->right->right;
 
                 free(temp_node);
             } else if (root->right->data=='T' || is_equivalent(root->left, root->right)) {
                 temp_node=root->left;
 
+                free_tree(root->right);
+
                 root->data=root->left->data;
-                root->left=root->left->left;
                 root->right=root->left->right;
+                root->is_negative=root->left->is_negative;
+                root->left=root->left->left;
 
                 free(temp_node);
             }
             break;
         case '|':
-            if (root->left->data=='T' || root->right->data=='T') {
+            if ((root->left->data=='T' || root->right->data=='T')) {
                 root->data='T';
                 free(root->left);
                 free(root->right);
@@ -148,18 +170,24 @@ void simplify_expression(node root) {
             } else if (root->left->data=='F') {
                 temp_node=root->right;
 
+                free(root->left);
+
                 root->data=root->right->data;
                 root->left=root->right->left;
+                root->is_negative=root->right->is_negative;
                 root->right=root->right->right;
 
                 free(temp_node);
             } else if (root->right->data=='F' || is_equivalent(root->left, root->right)) {
                 temp_node=root->left;
 
-                root->data=root->left->data;
-                root->left=root->left->left;
-                root->right=root->left->right;
+                free_tree(root->right);
 
+                root->data=root->left->data;
+                root->right=root->left->right;
+                root->is_negative=root->left->is_negative;
+                root->left=root->left->left;
+                
                 free(temp_node);
             }
             break;
@@ -201,6 +229,8 @@ int main() {
     printf("\n");
     simplify_expression(tree);
     print_tree(tree);
+
+    free_tree(tree);
 
     return 0;
 }
