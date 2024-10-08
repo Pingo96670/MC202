7d447168ff8d5a77120928f5067e726be9a9cd5a
Kauã Rodrigues <281279@dac.unicamp.br>
2024-10-08 01:50:05 -0300

    Correções/alterações: read_expression: Correção na atribuição de negação print_tree: Correção na impressão de parênteses e pontos de exclamação main: Implementação do input e correção dos três prints finais
    Adições:
    invert_negative: Função auxiliar da função de_morgan
    de_morgan: Implementação inicial

--- tarefa07/otimizar.c
+++ tarefa07/otimizar.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define MAX_STR_LEN 1024
 
 // Struct de um nó para a árvore
 typedef struct node_type *node;
@@ -14,24 +15,30 @@ struct node_type {
 
 
 node read_expression(char expression[], int *reading_pos, int is_negative) {
+    int negative_count=0;
     char current;
     node parent_temp, left_temp, right_temp, current_node_temp;
 
     current=expression[*reading_pos];
 
-    if (current=='!') {
-        is_negative==1;
+
+    while (current=='!') {
+        negative_count++;
         *reading_pos+=1;
         current=expression[*reading_pos];
     }
 
+    if (negative_count!=0) {
+        is_negative=negative_count%2;
+    }
+
     if (current=='(') {
         *reading_pos+=1;
-        left_temp=read_expression(expression, reading_pos, is_negative);
+        left_temp=read_expression(expression, reading_pos, 0);
         *reading_pos+=1;
         parent_temp=read_expression(expression, reading_pos, is_negative);
         *reading_pos+=1;
-        right_temp=read_expression(expression, reading_pos, is_negative);
+        right_temp=read_expression(expression, reading_pos, 0);
 
         parent_temp->left=left_temp;
         parent_temp->right=right_temp;
@@ -53,11 +60,48 @@ node read_expression(char expression[], int *reading_pos, int is_negative) {
     }
 }
 
+void invert_negative(int *is_negative) {
+    *is_negative=(*is_negative+1)%2;
+}
+
+void de_morgan(node root) {
+    if (root->is_negative && root->data=='&') {
+        root->data='|';
+        root->is_negative=0;
+        invert_negative(&(root->left->is_negative));
+        invert_negative(&(root->right->is_negative));
+    } else if (root->is_negative && root->data=='|') {
+        root->data='&';
+        root->is_negative=0;
+        invert_negative(&(root->left->is_negative));
+        invert_negative(&(root->right->is_negative));
+    }
+
+
+    if (root->left!=NULL) {
+        de_morgan(root->left);
+    }
+
+    if (root->right!=NULL) {
+        de_morgan(root->right);
+    }
+
+}
+
 void print_tree(node root) {
     if (root!=NULL) {
+        if(root->is_negative) {
+            printf("!");
+        }
+        if (root->data=='&' || root->data=='|') {
+            printf("(");
+        }
         print_tree(root->left);
         printf("%c", root->data);
         print_tree(root->right);
+        if (root->data=='&' || root->data=='|') {
+            printf(")");
+        }
     }
 }
 
@@ -65,11 +109,16 @@ void print_tree(node root) {
 int main() {
     int reading_pos=0;
     node tree;
-    char _;
+    char input[MAX_STR_LEN];
 
-    tree=read_expression("(a|b)", &reading_pos, 0);
+    scanf("%s", input);
+
+    tree=read_expression(input, &reading_pos, 0);
     
+    printf("%s\n", input);
+    de_morgan(tree);
     print_tree(tree);
+    printf("\n");
 
     return 0;
 }
