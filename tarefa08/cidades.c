3e003e23b7829296c86b7b5fd5fa15abb8373b21
Kauã Rodrigues <281279@dac.unicamp.br>
2024-10-15 03:06:09 -0300

    Correções/alterações: struct node_type: Adição de um atributo parent que talvez seja utilizado futuramente initialize_node: Correção no retorno e adição de alocação de memória insert_city: Implementação inicial main: Pequenas correções de erros e base do print para testes

--- tarefa08/cidades.c
+++ tarefa08/cidades.c
@@ -16,18 +16,22 @@ enum node_category {internal_node, free_leaf, occupied_leaf};
 typedef struct node_type *node;
 struct node_type {
     coords centre;
+    node parent;
     enum node_category category;
     union {
-        struct {node north_west; node north_east; node south_west; node south_east;};
-        struct {char city_name[MAX_CITY_LEN]; coords coord;};
+        struct internal_node {node north_west; node north_east; node south_west; node south_east;};
+        struct occupied_leaf {char city_name[MAX_CITY_LEN]; coords coord;};
     };
 };
 
 
-void initialize_node(node node, int size_x, int size_y) {
+node initialize_node(node node, int centre_x, int centre_y) {
+    node=malloc(sizeof(struct node_type));
     node->category=free_leaf;
-    node->centre.x=size_x/2;
-    node->centre.y=size_y/2;
+    node->centre.x=centre_x;
+    node->centre.y=centre_y;
+
+    return node;
 }
 
 // Recursive function to free a quaternary tree in postorder
@@ -52,21 +56,57 @@ void free_tree(node root) {
 }
 
 
-void insert_city(node tree, char city_name[MAX_CITY_LEN], int x, int y) {
-    switch (tree->category)
-    {
-    case internal_node:
-        /* code */
-        break;
+node insert_city(node root, char city_name[MAX_CITY_LEN], int x, int y, coords centre) {
+    char temp_name[MAX_CITY_LEN];
+    coords temp_coords;
+    node temp_node=root;
 
-    case free_leaf:
-        break;
+    switch (temp_node->category) {
+        case internal_node:
+            if (x<=temp_node->centre.x) {
+                centre.x/=2;
+                if (y<=temp_node->centre.y) {
+                    centre.y/=2;
+                    temp_node=insert_city(temp_node->south_west, city_name, x, y, centre);
+                } else {
+                    centre.y+=centre.y/2;
+                    temp_node=insert_city(temp_node->north_west, city_name, x, y, centre);
+                }
+            } else {
+                centre.x+=centre.x/2;
+                if (y<=temp_node->centre.y) {
+                    centre.y/=2;
+                    temp_node=insert_city(temp_node->south_east, city_name, x, y, centre);
+                } else {
+                    centre.y+=centre.y/2;
+                    temp_node=insert_city(temp_node->north_east, city_name, x, y, centre);
+                }
+            }
 
-    case occupied_leaf:
-        break;
-    
-    default:
-        assert(0);
+            break;
+
+        case free_leaf:
+            temp_node->category=occupied_leaf;
+            strcpy(temp_node->city_name, city_name);
+            break;
+
+        case occupied_leaf:
+            strcpy(temp_name, temp_node->city_name);
+            temp_coords=temp_node->coord;
+            temp_node->category=internal_node;
+
+            root->north_west=initialize_node(root->north_west, centre.x/2, centre.y+centre.y/2);
+            root->north_east=initialize_node(root->north_east, centre.x+centre.x/2, centre.y+centre.y/2);
+            root->south_west=initialize_node(root->south_west, centre.x/2, centre.y/2);
+            root->south_east=initialize_node(root->south_east, centre.x+centre.x/2, centre.y/2);
+
+            insert_city(temp_node, temp_name, temp_coords.x, temp_coords.y, temp_node->centre);
+            insert_city(temp_node, city_name, x, y, temp_node->centre);
+
+            break;
+        
+        default:
+            assert(0);
     } 
 }
 
@@ -122,21 +162,24 @@ int main() {
 
     scanf("%d", &dimensions);
 
-    initialize_node(tree, dimensions, dimensions);
+    tree=initialize_node(tree, dimensions/2, dimensions/2);
 
     while(!end_program) {
-        scanf("%c", &command);
+        scanf(" %c", &command);
 
         switch (command) {
             case 'i':
                 scanf("%d %d %s", &coord_x, &coord_y, city_name);
 
-                insert_city(tree, city_name, coord_x, coord_y);
+                insert_city(tree, city_name, coord_x, coord_y, tree->centre);
 
                 break;
 
             case 'b':
-                //
+                scanf("%d %d", &coord_x, &coord_y);
+
+                printf(search_in_point(tree, coord_x, coord_y)->city_name);
+
                 break;
 
             case 'o':
