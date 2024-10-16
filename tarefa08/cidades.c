c8b351f1009f1488aa329c21939e163d444fb271
Kauã Rodrigues <281279@dac.unicamp.br>
2024-10-16 01:14:34 -0300

    Correções/alterações: insert_city: Correção no cálculo do centro; alterações relacionadas ao uso do atributo parent main: Alterações relacionadas ao uso do atributo parent; correção de typo na comparação durante a busca por ponto; implementação do comando de remoção
    Adições:
    branch_collapse_check: Implementação inicial como função auxiliar de remove_from_point
    collapse_branch: Implementação inicial como função auxiliar de remove_from_point
    remove_from_point: Implementação inicial

--- tarefa08/cidades.c
+++ tarefa08/cidades.c
@@ -60,30 +60,40 @@ void free_tree(node root) {
 }
 
 
-node insert_city(node root, char city_name[MAX_CITY_LEN], int x, int y, float_coords centre) {
+node insert_city(node root, char city_name[MAX_CITY_LEN], int x, int y, float_coords centre, node parent_node) {
+    float centre_refactor;
     char temp_name[MAX_CITY_LEN];
     coords temp_coords;
     node temp_node=root;
 
+    if (parent_node==NULL) {
+        centre_refactor=centre.x/2.0;
+    } else {
+        centre_refactor=abs(centre.x-parent_node->centre.x)/2.0;
+        if (centre_refactor<abs(centre.y-parent_node->centre.y)/2.0) {
+            centre_refactor=abs(centre.y-parent_node->centre.y)/2.0;
+        }
+    }
+
     switch (temp_node->category) {
         case internal_node:
             if (x<=temp_node->centre.x) {
-                centre.x/=2;
+                centre.x-=centre_refactor;
                 if (y<=temp_node->centre.y) {
-                    centre.y/=2;
-                    temp_node=insert_city(temp_node->south_west, city_name, x, y, centre);
+                    centre.y-=centre_refactor;
+                    temp_node=insert_city(temp_node->south_west, city_name, x, y, centre, temp_node);
                 } else {
-                    centre.y+=centre.y/2;
-                    temp_node=insert_city(temp_node->north_west, city_name, x, y, centre);
+                    centre.y+=centre_refactor;
+                    temp_node=insert_city(temp_node->north_west, city_name, x, y, centre, temp_node);
                 }
             } else {
-                centre.x+=centre.x/2;
+                centre.x+=centre_refactor;
                 if (y<=temp_node->centre.y) {
-                    centre.y/=2;
-                    temp_node=insert_city(temp_node->south_east, city_name, x, y, centre);
+                    centre.y-=centre_refactor;
+                    temp_node=insert_city(temp_node->south_east, city_name, x, y, centre, temp_node);
                 } else {
-                    centre.y+=centre.y/2;
-                    temp_node=insert_city(temp_node->north_east, city_name, x, y, centre);
+                    centre.y+=centre_refactor;
+                    temp_node=insert_city(temp_node->north_east, city_name, x, y, centre, temp_node);
                 }
             }
 
@@ -94,6 +104,7 @@ node insert_city(node root, char city_name[MAX_CITY_LEN], int x, int y, float_co
             strcpy(temp_node->city_name, city_name);
             temp_node->city_coords.x=x;
             temp_node->city_coords.y=y;
+            temp_node->parent=parent_node;
             break;
 
         case occupied_leaf:
@@ -101,13 +112,13 @@ node insert_city(node root, char city_name[MAX_CITY_LEN], int x, int y, float_co
             temp_coords=temp_node->city_coords;
             temp_node->category=internal_node;
 
-            root->north_west=initialize_node(root->north_west, centre.x/2, centre.y+centre.y/2);
-            root->north_east=initialize_node(root->north_east, centre.x+centre.x/2, centre.y+centre.y/2);
-            root->south_west=initialize_node(root->south_west, centre.x/2, centre.y/2);
-            root->south_east=initialize_node(root->south_east, centre.x+centre.x/2, centre.y/2);
+            root->north_west=initialize_node(root->north_west, centre.x-centre_refactor, centre.y+centre_refactor);
+            root->north_east=initialize_node(root->north_east, centre.x+centre_refactor, centre.y+centre_refactor);
+            root->south_west=initialize_node(root->south_west, centre.x-centre_refactor, centre.y-centre_refactor);
+            root->south_east=initialize_node(root->south_east, centre.x+centre_refactor, centre.y-centre_refactor);
 
-            insert_city(temp_node, temp_name, temp_coords.x, temp_coords.y, temp_node->centre);
-            insert_city(temp_node, city_name, x, y, temp_node->centre);
+            insert_city(temp_node, temp_name, temp_coords.x, temp_coords.y, temp_node->centre, temp_node->parent);
+            insert_city(temp_node, city_name, x, y, temp_node->centre, temp_node->parent);
 
             break;
         
@@ -187,8 +198,81 @@ void search_in_region(node node, int circle_x, int circle_y, int radius) {
     }
 }
 
-void remove_from_point() {
+int branch_collapse_check(node node) {
+    int occupied_leaf_count=0, free_leaf_count=0;
 
+    if (node->north_west->category==occupied_leaf) {
+        occupied_leaf_count++;
+    } else if (node->north_west->category==free_leaf) {
+        free_leaf_count++;
+    }
+
+    if (node->north_east->category==occupied_leaf) {
+        occupied_leaf_count++;
+    } else if (node->north_east->category==free_leaf) {
+        free_leaf_count++;
+    }
+
+    if (node->south_west->category==occupied_leaf) {
+        occupied_leaf_count++;
+    } else if (node->south_west->category==free_leaf) {
+        free_leaf_count++;
+    }
+
+    if (node->south_east->category==occupied_leaf) {
+        occupied_leaf_count++;
+    } else if (node->south_east->category==free_leaf) {
+        free_leaf_count++;
+    }
+
+    return (occupied_leaf_count==1 && free_leaf_count==3);
+}
+
+void collapse_branch(node node) {
+    char temp_name[MAX_CITY_LEN];
+    struct node_type *only_occupied_node;
+    coords temp_coords;
+
+    node->category=occupied_leaf;
+
+    if (node->north_west->category==occupied_leaf) {
+        only_occupied_node=node->north_west;
+    }
+
+    if (node->north_east->category==occupied_leaf) {
+        only_occupied_node=node->north_east;
+    }
+
+    if (node->south_west->category==occupied_leaf) {
+        only_occupied_node=node->south_west;
+    }
+
+    if (node->south_east->category==occupied_leaf) {
+        only_occupied_node=node->south_east;
+    }
+
+    strcpy(temp_name, only_occupied_node->city_name);
+    temp_coords=only_occupied_node->city_coords;
+
+    free(node->north_west);
+    free(node->north_east);
+    free(node->south_west);
+    free(node->south_east);
+
+    strcpy(node->city_name, temp_name);
+    node->city_coords=temp_coords;
+
+    if (node->parent!=NULL && branch_collapse_check(node->parent)) {
+        collapse_branch(node->parent);
+    }
+}
+
+void remove_from_point(node node_to_remove, int x, int y) {
+    node_to_remove->category=free_leaf;
+
+    if (node_to_remove->parent!=NULL && branch_collapse_check(node_to_remove->parent)) {
+        collapse_branch(node_to_remove->parent);
+    }
 }
 
 void print_from_point() {
@@ -204,6 +288,7 @@ int main() {
     scanf("%d", &dimensions);
 
     tree=initialize_node(tree, (dimensions+1)/2, (dimensions+1)/2);
+    tree->parent=NULL;
 
     while(!end_program) {
         scanf(" %c", &command);
@@ -212,7 +297,7 @@ int main() {
             case 'i':
                 scanf("%d %d %s", &coord_x, &coord_y, city_name);
 
-                insert_city(tree, city_name, coord_x, coord_y, tree->centre);
+                insert_city(tree, city_name, coord_x, coord_y, tree->centre, NULL);
 
                 printf("Cidade %s inserida no ponto (%d,%d).\n", city_name, coord_x, coord_y);
 
@@ -223,7 +308,7 @@ int main() {
 
                 found_point=search_in_point(tree, coord_x, coord_y);
 
-                if (found_point->category==occupied_leaf && (found_point->city_coords.x=coord_x && found_point->city_coords.y==coord_y)) {
+                if (found_point->category==occupied_leaf && (found_point->city_coords.x==coord_x && found_point->city_coords.y==coord_y)) {
                     printf("Cidade %s encontrada no ponto (%d,%d).\n", found_point->city_name, coord_x, coord_y);
                 } else {
                     printf("Nenhuma cidade encontrada no ponto (%d,%d).\n", coord_x, coord_y);
@@ -241,20 +326,27 @@ int main() {
                 break;
 
             case 'r':
-                //
+                scanf("%d %d", &coord_x, &coord_y);
+
+                found_point=search_in_point(tree, coord_x, coord_y);
+
+                printf("Cidade %s removida do ponto (%d,%d).\n", found_point->city_name, coord_x, coord_y);
+
+                remove_from_point(found_point, coord_x, coord_y);
+
                 break;
 
             case 'p':
                 break;
 
             case 's':
-                printf("Sistema encerrado.");
+                printf("Sistema encerrado.\n");
                 free_tree(tree);
                 end_program=1;
                 break;
             
             default:
-                printf("Comando não reconhecido");
+                printf("Comando não reconhecido\n");
                 break;
         }
     }
